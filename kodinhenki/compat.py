898601e086f98277dd994d14baea56f969122082
Markus Stenberg <markus.stenberg@iki.fi>
2014-09-23 13:06:27 +0300

    Added socketserver to compat.

--- kodinhenki/compat.py
+++ kodinhenki/compat.py
@@ -9,8 +9,8 @@
 # Copyright (c) 2014 Markus Stenberg
 #
 # Created:       Mon Sep 22 19:06:38 2014 mstenber
-# Last modified: Mon Sep 22 19:06:51 2014 mstenber
-# Edit time:     1 min
+# Last modified: Tue Sep 23 11:56:24 2014 mstenber
+# Edit time:     3 min
 #
 """
 
@@ -19,8 +19,15 @@ Python 2.x / 3.x compatibility module
 """
 
 try:
+    # 2.x-ism
     import Queue
 except ImportError:
     import queue as Queue
 import kodinhenki.util
+
+try:
+    # 2.x-ism
+    import SocketServer
+except ImportError:
+    import socketserver as SocketServer
 
