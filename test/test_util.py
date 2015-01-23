76ccf5de4db162cba30f2f3acc60b2be79eec4e9
Markus Stenberg <markus.stenberg@iki.fi>
2015-01-23 12:06:19 +0200

    Moved signal tests to prdb.

--- test/test_util.py
+++ test/test_util.py
@@ -9,8 +9,8 @@
 # Copyright (c) 2014 Markus Stenberg
 #
 # Created:       Mon Sep 22 19:04:40 2014 mstenber
-# Last modified: Tue Sep 23 11:53:09 2014 mstenber
-# Edit time:     2 min
+# Last modified: Thu Jan 22 14:38:41 2015 mstenber
+# Edit time:     3 min
 #
 """
 
@@ -19,21 +19,6 @@ Tests for util.py
 """
 
 import kodinhenki.util
-from mock import Mock
-
-def test_signal():
-    s = kodinhenki.util.Signal()
-    m1 = Mock()
-    m2 = Mock()
-    s.connect(m1)
-    s.connect(m2)
-    s()
-    assert m1.called and m2.called
-    m1.reset_mock()
-    m2.reset_mock()
-    s.disconnect(m2)
-    s()
-    assert m1.called and not m2.called
 
 def test_get_ip_address():
     a4 = kodinhenki.util.get_ipv4_address()
