40d9347268c2891788ffd598e206169e9c3aac9d
Markus Stenberg <markus.stenberg@iki.fi>
2015-08-22 11:42:59 +0300

    sync: Started backwards compatible sync replacement implementation that leverages pysyma+SHSP instead of silly TCP socket.

--- kodinhenki/sync.py
+++ kodinhenki/sync.py
@@ -9,8 +9,8 @@
 # Copyright (c) 2014 Markus Stenberg
 #
 # Created:       Wed Oct  1 13:15:48 2014 mstenber
-# Last modified: Fri Aug 21 10:39:58 2015 mstenber
-# Edit time:     127 min
+# Last modified: Sat Aug 22 11:41:55 2015 mstenber
+# Edit time:     158 min
 #
 """
 
@@ -35,9 +35,8 @@ import prdb
 import kodinhenki as kh
 from kodinhenki.util import Signal
 import kodinhenki.prdb_kh as _prdb
-
-import kodinhenki.compat as compat
-_socketserver = compat.get_socketserver()
+import pysyma.shsp
+import pysyma.si
 
 import threading
 import socket
@@ -54,135 +53,68 @@ in_sync = Signal()
 
 BY='sync'
 
-class SyncWriter:
-    stopped = False
-    def __init__(self, f):
-        self.f = f
-        self.queue = []
-        self.semaphore = threading.Semaphore()
-        self.start()
-    def start(self):
-        self.t = threading.Thread(target=self.write_loop)
-        self.t.daemon = True
-        self.t.start()
-    def stop(self):
-        self.stopped = True
-    def write(self, x):
-        self.queue.append(x)
-        self.semaphore.release()
-    def write_loop(self):
-        while not self.stopped:
-            while self.queue:
-                s = self.queue.pop(0)
-                try:
-                    self.f.write(s)
-                except:
-                    return
-            self.semaphore.acquire()
-
-
-# Note: This has to be in it's own thread or bad things happen ..
-class SyncReceiver(_socketserver.StreamRequestHandler, prdb.Writer):
-    def setup(self):
-        _socketserver.StreamRequestHandler.setup(self)
-        self.writer = SyncWriter(self.wfile)
-        self.server.add_receiver(self)
-        _debug('initialized %s', self)
-    def handle(self):
-        db = self.server.db
-        while True:
-            try:
-                line = self.rfile.readline().decode('utf-8').strip()
-            except:
-                return
-            if not line:
-                break
-            _debug('handling %s', line)
-            d = json.loads(line)
-            if d[0] == 'log':
-                with _prdb.lock:
-                    db.process_decoded_line(d[1], by=BY)
-            elif d[0] == 'sync_end':
-                in_sync()
-            else:
-                raise NotImplementedError('unknown input', d)
-            request_handled()
-    def handle_flushed_data(self, data):
-        self.send_update_one('log', data)
-    def finish(self):
-        self.server.remove_receiver(self)
-        _debug('finished %s', self)
-    def send_update_one(self, *args):
-        s = json.dumps(args)
-        _debug('sending %s', s)
-        b = s.encode('utf-8') + b'\n'
-        self.writer.write(b)
-
-class SyncServer(_socketserver.ThreadingMixIn, _socketserver.TCPServer):
-    # .. for the socketserver superclasses ..
-    allow_reuse_address = True
-    daemon_threads = True
-
-    def __init__(self, db, client, *args, **kwargs):
+class Syncer(pysyma.shsp.SHSPSubscriber):
+    def __init__(self, db, p):
         self.db = db
-        self._receivers = []
-        self.remove_receiver = self._receivers.remove
-        self.client = client
-        if not client:
-            _socketserver.TCPServer.__init__(self, *args, **kwargs)
-        self.db.object_changed.connect(self.db_object_changed)
-        #self.db.object_removed.connect(self.db_object_removed)
-    def add_receiver(self, r):
-        with _prdb.lock:
-            self._receivers.append(r)
-            self.db.commit()
-            self.db.dump_to_writer(r)
-        r.send_update_one('sync_end')
+        self.p = p
+        # local -> remote
+        db.object_changed.connect(self.db_object_changed)
+        p.add_subscriber(self)
+    def network_consistent_event(self, c):
+        if c:
+            in_sync()
     def db_object_changed(self, o, key, by, when, old, new):
-        if self.client and by == BY: return
+        if by == BY: return
+        skey = '%s/%s' % (o, key)
+        self.p.update_dict({skey : new and new or None}, ts=when)
+    def dict_update_event(self, n, od, nd):
+        if n is self.p.own_node: return
+        for sk, (ts, v) in nd.items():
+            if not k.startswith('.kh.'): continue
+            oid, k = sk.split('/')
+            line = (ts, oid, {k: v})
+            db.process_decoded_line(line, by=BY)
+        for sk in od.keys().difference(nd.keys()):
+            if not k.startswith('.kh.'): continue
+            oid, k = sk.split('/')
+            line = (ts, oid, {k: None})
+            db.process_decoded_line(line, by=BY)
+        request_handled()
 
-        # Wonder if these should be buffered.. nahh..
-        for r in self._receivers:
-            r.send_update_one('log', (when, o.id, {key: new}))
+def _shared_start(db, si, p, **kw):
+    sync = Syncer(db, p)
+    t = threading.Thread(target=si.loop)
+    t.daemon = True
+    t.start()
+    return dict(si=si, p=p, t=t, sync=sync, **kw)
 
+def start(db):
+    si = pysyma.si.HNCPSystemInterface()
+    p = si.create_dncp(pysyma.shsp.SHSP)
+    return _shared_start(db, si, p)
 
-def start_server(db=None, ip='', port=kh.PORT):
+def stop(s):
+    s['si'].stop()
+    # TBD: Should we close sockets too?
+
+# TBD: Should these backwards compatible APIs be killed?
+def start_server(db=None, ip='::1', port=kh.PORT):
     db = db or kh.get_database()
-    server = SyncServer(db, False, (ip, port), SyncReceiver)
-    st = threading.Thread(target=server.serve_forever)
-    st.daemon = True
-    st.start()
-    return {'thread': st, 'server': server, 'port': server.server_address[1]}
+    si = pysyma.si.HNCPSystemInterface()
+    # ip is ignored
+    s = si.create_socket(port=port)
+    p = pysyma.shsp.SHSP(sys=s)
+    s.set_dncp_unicast_listen(p)
+    return _shared_start(db, si, p, port=s.get_port())
 
 def start_client(db, addr):
-    server = SyncServer(db, True)
-    o = {'alive': True}
-    def _f():
-        while o['alive']:
-            try:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                sock.connect(addr)
-                o['sock'] = sock
-            except:
-                _error("socket-connect failed, retrying in a second")
-                time.sleep(1)
-                continue
-            SyncReceiver(sock, None, server)
-            _error("client connection died(?), retrying in a second")
-            time.sleep(1)
-    ct = threading.Thread(target=_f)
-    ct.daemon = True
-    ct.start()
-    o['thread'] = ct
-    return o
+    if addr[0] == '127.0.0.1':
+        addr = ('::1', addr[1])
+    si = pysyma.si.HNCPSystemInterface()
+    s = si.create_socket(port=0)
+    p = pysyma.shsp.SHSP(sys=s)
+    s.set_dncp_unicast_connect(p, addr)
+    return _shared_start(db, si, p)
 
-def stop_server(s):
-    s['server'].shutdown()
-
-def stop_client(s):
-    s['alive'] = False
-    sock = s.get('sock', None)
-    if sock:
-        sock.close()
-    # TBD: is there some way to force faster closure of client?
-
+stop_client = stop
+stop_server = stop
