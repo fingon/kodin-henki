ddc3d6e81b419b8eb271ed271cda17286fb25cfb
Markus Stenberg <markus.stenberg@iki.fi>
2015-08-24 11:20:40 +0300

    process: Added optional kwarg passing to change e.g. update interval.

--- kodinhenki/process.py
+++ kodinhenki/process.py
@@ -9,8 +9,8 @@
 # Copyright (c) 2014 Markus Stenberg
 #
 # Created:       Wed Oct  1 15:10:38 2014 mstenber
-# Last modified: Wed Jan 28 22:04:10 2015 mstenber
-# Edit time:     17 min
+# Last modified: Mon Aug 24 10:24:24 2015 mstenber
+# Edit time:     18 min
 #
 """
 
@@ -25,8 +25,8 @@ import kodinhenki.updater as updater
 import kodinhenki.prdb_kh as _prdb_kh
 
 import os
-
 import logging
+
 logger = logging.getLogger(__name__)
 _debug = logger.debug
 _error = logger.error
@@ -34,13 +34,15 @@ _error = logger.error
 PS_STRING = 'ps awwx'
 
 class ProcessMonitor(updater.Updated):
-    def __init__(self, pdict):
+    update_interval = 30
+    def __init__(self, pdict, **kw):
         self.pdict = pdict
+        self.__dict__.update(**kw)
         # Add the objects
         for name in pdict.keys():
             _prdb_kh.Process.new_named(name, on=False)
     def next_update_in_seconds(self):
-        return 30 # hardcoded, but oh well
+        return self.update_interval
     def update(self):
         with os.popen(PS_STRING, 'r') as psfile:
             self.update_lines(list(psfile))
@@ -51,8 +53,8 @@ class ProcessMonitor(updater.Updated):
                 on = True if any (match in l for l in lines) else False
                 _prdb_kh.Process.get_named(name).on=on
 
-def start(d):
+def start(d, **kw):
     _debug('starting process %s', repr(d))
-    m = ProcessMonitor(d)
+    m = ProcessMonitor(d, **kw)
     updater.add(m)
     return m
